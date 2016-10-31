3885d8f8a1e4462b68f3c61ca502a98fb83d566c
katjusza <kat_w@vp.pl>
2016-10-31 20:40:33 +0100

    Added chains classes

---
+++ src/ToNotGiveAFuck/Models/TODOs/ChainsTodos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToNotGiveAFuck.Models.TODOs
+{
+    public class ChainsTodos
+    {
+        [Key, Column(Order = 1)]
+        public int ChainID { get; set; }
+        public virtual Chain Chain { get; set; }
+        [Key, Column(Order = 2)]
+        public Guid TodoId { get; set; }
+        public virtual TODO Todo { get; set; }
+        public int Position { get; set; }
+
+    }
+}
